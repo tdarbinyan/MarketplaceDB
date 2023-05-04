fa96e2f1cfcdede1b51cb556522c81628fa559f9
darbinjan telemak <darbinian.ta@phystech.edu>
2023-05-04 21:57:22 +0000

    Upload New File

---
+++ tests/test_view2.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+
+def test_orders_mask_view():
+
+    # Открываем соединение с базой данных
+    conn = psycopg2.connect("marketplace.db")
+
+    # Создаем курсор
+    cur = conn.cursor()
+
+    # Выполняем запрос
+    cur.execute("SELECT id, user_id, masked_status, date FROM orders_mask")
+
+    # Получаем результаты
+    results = cur.fetchall()
+
+    # Закрываем соединение
+    conn.close()
+
+    # Проверяем, что запрос вернул не пустой результат
+    assert len(results) > 0
+
+    # Проверяем, что все поля в результате имеют значения
+    for row in results:
+        assert row[0] is not None
+        assert row[1] is not None
+        assert row[2] is not None
+        assert row[3] is not None
